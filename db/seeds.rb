5239beb8c29b204bd62377c4bedfc8e32712452b
Zack Fisch <zack@zfisch.com>
2014-06-28 13:35:27 -0400

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,24 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Picture.create!(
+    :title  => "The old church on the coast of White sea",
+    :artist => "Sergey Ershov",
+    :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+  )
+
+Picture.create!(
+    :title  => "Sea Power",
+    :artist => "Stephen Scullion",
+    :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"  
+  )
+
+Picture.create!(
+    :title  => "Into the Poppies",
+    :artist => "John Wilhelm",
+    :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"  
+  )
